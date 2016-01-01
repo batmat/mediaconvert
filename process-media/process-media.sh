c17d21a4f2b1c9efa026fd08190cbbc9a0b47244
Baptiste Mathus <batmat@batmat.net>
2016-01-01 22:14:10 +0100

    Processing for .MP4 from Powershot G1X Mark II
    Videos are full-HD (1080p + aac encoded), but surprisingly reencoding
    with libx264 seems to divide by ~5 the size of the video w/o apparently
    reducing the video quality. Keeping the original audio as is.

--- process-media/process-media.sh
+++ process-media/process-media.sh
@@ -11,3 +11,11 @@ do
  avconv -i "$i" -c:v libx264 -c:a mp3 -ar 44100 "$mp4name"
  touch -a -m -r "$i" "$mp4name"
 done
+
+for i in *.MP4
+do
+ mp4name=$(echo "$i"|cut -d"." -f1).mp4
+ echo "Converting $i into $mp4name"
+ avconv -i "$i" -c:v libx264 -c:a copy "$mp4name"
+ touch -a -m -r "$i" "$mp4name"
+done

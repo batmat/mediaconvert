0c2c3383dc2b916e69d00e770abac88c03d6c42b
Baptiste Mathus <batmat@batmat.net>
2016-09-20 22:00:41 +0200

    Support .AVI if they're present

--- process-media/process-media.sh
+++ process-media/process-media.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-set -eou pipefail
+set -ou pipefail
 
-exifautotran *.JPG
+if ls *.JPG > /dev/null 2>&1 ; then
+  echo "Reset image orientation."
+  set -e
+  exifautotran *.JPG
+  set +e
+else
+  echo "No image found."
+fi
 rm -f *.THM
 
-for i in *.AVI
-do
- mp4name=$(echo "$i"|cut -d"." -f1).mp4
- echo "Converting $i into $mp4name"
- avconv -i "$i" -c:v libx264 -c:a mp3 -ar 44100 "$mp4name"
- touch -a -m -r "$i" "$mp4name"
-done
+if ls *.AVI > /dev/null 2>&1 ; then
+  for i in *.AVI
+  do
+   set -e
+   mp4name=$(echo "$i"|cut -d"." -f1).mp4
+   echo "Converting $i into $mp4name"
+   avconv -i "$i" -c:v libx264 -c:a mp3 -ar 44100 "$mp4name"
+   touch -a -m -r "$i" "$mp4name"
+   set +e
+  done
 
-for i in *.MP4
-do
- mp4name=$(echo "$i"|cut -d"." -f1).mp4
- echo "Converting $i into $mp4name"
- avconv -i "$i" -c:v libx264 -c:a copy "$mp4name"
- touch -a -m -r "$i" "$mp4name"
-done
+else
+  echo "No .AVI found."
+fi
+
+if ls *.MP4  > /dev/null 2>&1 ; then
+  for i in *.MP4
+  do
+   set -e
+   mp4name=$(echo "$i"|cut -d"." -f1).mp4
+   echo "Converting $i into $mp4name"
+   avconv -i "$i" -c:v libx264 -c:a copy "$mp4name"
+   touch -a -m -r "$i" "$mp4name"
+  done
+else
+  echo "No .MP4 found."
+fi

1e8830d22078784d3a4cba7fe3ad214c2e913c02
Baptiste Mathus <batmat@batmat.net>
2016-11-05 17:26:05 +0100

    More sensible replacement with sed
    Without that, any dot in the middle before the extension would just
    screw up everything...

--- process-media/process-media.sh
+++ process-media/process-media.sh
@@ -15,7 +15,7 @@ if ls *.AVI > /dev/null 2>&1 ; then
   for i in *.AVI
   do
    set -e
-   mp4name=$(echo "$i"|cut -d"." -f1).mp4
+   mp4name=$(echo "$i"|sed 's/.AVI$//').mp4
    echo "Converting $i into $mp4name"
    avconv -i "$i" -c:v libx264 -c:a mp3 -ar 44100 "$mp4name"
    touch -a -m -r "$i" "$mp4name"
@@ -30,7 +30,7 @@ if ls *.MP4  > /dev/null 2>&1 ; then
   for i in *.MP4
   do
    set -e
-   mp4name=$(echo "$i"|cut -d"." -f1).mp4
+   mp4name=$(echo "$i"|sed 's/.MP4$//').mp4
    echo "Converting $i into $mp4name"
    avconv -i "$i" -c:v libx264 -c:a copy "$mp4name"
    touch -a -m -r "$i" "$mp4name"
